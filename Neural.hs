f8b7245fe352a2c96c9457e3c8a0b7e923d4a317
Giorgio Giuffrè <giorgiogiuffre23@gmail.com>
2019-12-29 20:37:11 +0100

    improve inline docs

--- Neural.hs
+++ Neural.hs
@@ -2,45 +2,60 @@ module Neural
 ( Network
 , Layer
 , infer
-, analyze
-, getErrors
-, sgdEpoch
 , sgdUpdates
-, shuffle
+, performance
 ) where
+
+
 
 import System.Random (randomRIO)
 import Data.List (genericLength)
 import Data.Matrix (Matrix, elementwise, fromLists, toLists, transpose, scaleMatrix)
 
-type BiasMatrix t = Matrix t
-type WeightsMatrix t = Matrix t
-type Layer t = (BiasMatrix t, WeightsMatrix t)
+
+
+-- a deep neural network is a sequence of layers
 type Network t = [Layer t]
 
+-- each layer of a network has biases and weights
+type Layer t = (BiasMatrix t, WeightsMatrix t)
+
+-- the biases and weights of a layer can be represented as matrices
+type BiasMatrix t = Matrix t
+type WeightsMatrix t = Matrix t
+
+-- a dataset is a list of input-output pairs (input first, label second)
+type Dataset t = [(Matrix t, Matrix t)]
+
+
+
+-- list of matrices containing biases, layer by layer
+biases :: Network t -> [BiasMatrix t]
 biases net = map fst net
+
+-- list of matrices containing weights, layer by layer
+weights :: Network t -> [BiasMatrix t]
 weights net = map snd net
 
--- sigmoid activation function
+-- sigmoid of a given number
 sigmoid :: (Floating t) => t -> t
 sigmoid x = 1.0 / (1.0 + exp (-x))
 
--- derivative of the sigmoid activation function
+-- derivative of the sigmoid at a given value
 sigmoid' :: (Floating t) => t -> t
 sigmoid' x = sigmoid x * (1.0 - sigmoid x)
 
--- apply a function to each element of a matrix
+-- result of applying a function to each element of a matrix
 foreach :: (t -> t) -> Matrix t -> Matrix t
 foreach func mat = fromLists $ map (map func) $ toLists mat
 
--- quadratic cost function
+-- quadratic cost function of a prediction, given a label (ground truth)
 quadCost :: (Floating t) => Matrix t -> Matrix t -> t
-quadCost out label = sum (foreach (^2) (label - out)) / 2
+quadCost prediction label = sum (foreach (^2) (label - prediction)) / 2
 
--- derivative of the quadratic cost function w.r.t.
--- the activation of one output neuron
+-- derivative of the quadratic cost function w.r.t. a predicted value
 quadCost' :: (Floating t) => Matrix t -> Matrix t -> Matrix t
-quadCost' out label = out - label
+quadCost' prediction label = prediction - label
 
 -- output of a neural network given some input
 infer :: (Floating t) => Matrix t -> Network t -> Matrix t
@@ -79,20 +94,21 @@ gradientWeights inputs errors = layerGradient:nextGradients
 gradientBiases :: (Floating t) => [Matrix t] -> [Matrix t] -> [Matrix t]
 gradientBiases inputs errors = errors
 
--- error of the weighted inputs of a network w.r.t. a cost function
-getErrors :: (Floating t) => Network t -> [(Matrix t, Matrix t)] -> Matrix t -> [Matrix t]
-getErrors (l:[]) states label = [errWdInput]
+-- partial derivatives of a cost function w.r.t. the weighted inputs of layers in a network
+deltas :: (Floating t) => Network t -> [(Matrix t, Matrix t)] -> Matrix t -> [Matrix t]
+deltas (l:[]) states label = [errWdInput]
     where errWdInput = elementwise (*) errActivation (foreach sigmoid' zs)
           errActivation = (as - label)
           (zs, as) = head states
-getErrors net states label = errWdInput:nextErrs
+deltas net states label = errWdInput:nextErrs
     where errWdInput = elementwise (*) errActivation (foreach sigmoid' zs)
           errActivation = transpose nextWs * (head nextErrs)
-          nextErrs = getErrors (tail net) (tail states) label
+          nextErrs = deltas (tail net) (tail states) label
           (nextBs, nextWs) = head $ tail net
           (zs, as) = head states
 
-sgdEpoch :: (Floating t) => Network t -> [(Matrix t, Matrix t)] -> t -> [Network t]
+-- list of networks whose parameters are updated with SGD throughout an epoch
+sgdEpoch :: (Floating t) => Network t -> Dataset t -> t -> [Network t]
 sgdEpoch net [] eta = []
 sgdEpoch net dataset eta = newNet:nextUpdates
     where nextUpdates = sgdEpoch newNet (tail dataset) eta
@@ -102,24 +118,25 @@ sgdEpoch net dataset eta = newNet:nextUpdates
           matsDiff (m1, m2) = m1 - m2
           correctionsB = map (scaleMatrix eta) (gradientBiases  activs errors)
           correctionsW = map (scaleMatrix eta) (gradientWeights activs errors)
-          errors = getErrors net states label
+          errors = deltas net states label
           input  = fst $ head dataset
           label  = snd $ head dataset
           activs = input:(map snd states)
           states = analyze input net
 
--- infinite list of networks whose parameters are changed with SGD
-sgdUpdates :: (Floating t) => Network t -> [(Matrix t, Matrix t)] -> t -> [Network t]
+-- infinite list of networks whose parameters are changed with SGD throughout (infinite) epochs
+sgdUpdates :: (Floating t) => Network t -> Dataset t -> t -> [Network t]
 sgdUpdates net dataset eta = newNet:nextNets
     where newNet = last $ sgdEpoch net dataset eta
           nextNets = sgdUpdates newNet dataset eta
 
--- average performance of a network on a dataset
-performance :: (Floating t) => Network t -> [(Matrix t, Matrix t)] -> t
+-- average quadratic cost of a network on a dataset
+performance :: (Floating t) => Network t -> Dataset t -> t
 performance net dataset = sum costs / genericLength costs
     where costs = [cost input label | (input, label) <- dataset]
           cost input label = quadCost (infer input net) label
 
+-- TODO
 shuffle x = if length x < 2 then return x else do
     i <- System.Random.randomRIO (0, length(x)-1)
     r <- shuffle (take i x ++ drop (i+1) x)
