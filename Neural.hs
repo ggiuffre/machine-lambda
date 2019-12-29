9f65c53f17ea3c2e767039f44a00684cbb65a3aa
Giorgio Giuffrè <giorgiogiuffre23@gmail.com>
2019-12-29 19:58:46 +0100

    improve Network data structure

--- Neural.hs
+++ Neural.hs
@@ -1,16 +1,25 @@
 module Neural
-( Network (..)
+( Network
+, Layer
 , infer
+, analyze
+, getErrors
 , sgdEpoch
+, sgdUpdates
+, shuffle
 ) where
 
 import System.Random (randomRIO)
 import Data.List (genericLength)
 import Data.Matrix (Matrix, elementwise, fromLists, toLists, transpose, scaleMatrix)
 
-data Network t = Network { biases :: [Matrix t]
-                         , weights :: [Matrix t]
-                         } deriving (Show)
+type BiasMatrix t = Matrix t
+type WeightsMatrix t = Matrix t
+type Layer t = (BiasMatrix t, WeightsMatrix t)
+type Network t = [Layer t]
+
+biases net = map fst net
+weights net = map snd net
 
 -- sigmoid activation function
 sigmoid :: (Floating t) => t -> t
@@ -35,29 +44,27 @@ quadCost' out label = out - label
 
 -- output of a neural network given some input
 infer :: (Floating t) => Matrix t -> Network t -> Matrix t
-infer input net = foldl activation input layers
-    where layers = zip (weights net) (biases net)
+infer input net = foldl activation input net
 
 -- activation and w.ed input of each layer, given an input to the whole network
 analyze :: (Floating t) => Matrix t -> Network t -> [(Matrix t, Matrix t)]
 analyze input net = zip zs as
-    where layers = zip (weights net) (biases net)
-          zs = wdInputs input layers
-          as = tail $ scanl activation input layers
+    where zs = wdInputs input net
+          as = tail $ scanl activation input net
 
 -- weighted input of each layer, given an input to the whole network
-wdInputs :: (Floating t) => Matrix t -> [(Matrix t, Matrix t)] -> [Matrix t]
+wdInputs :: (Floating t) => Matrix t -> Network t -> [Matrix t]
 wdInputs input [] = []
-wdInputs input layers = z:(wdInputs (foreach sigmoid z) (tail layers))
-    where z = wInput input (head layers)
+wdInputs input net = z:(wdInputs (foreach sigmoid z) (tail net))
+    where z = wInput input (head net)
 
 -- activation of a layer given input, weights, and biases
-activation :: (Floating t) => Matrix t -> (Matrix t, Matrix t) -> Matrix t
-activation input (weights, biases) = foreach sigmoid (biases + weights * input)
+activation :: (Floating t) => Matrix t -> Layer t -> Matrix t
+activation input (biases, weights) = foreach sigmoid (biases + weights * input)
 
 -- weighted input of a layer given input, weights, and biases
-wInput :: (Floating t) => Matrix t -> (Matrix t, Matrix t) -> Matrix t
-wInput input (weights, biases) = biases + weights * input
+wInput :: (Floating t) => Matrix t -> Layer t -> Matrix t
+wInput input (biases, weights) = biases + weights * input
 
 -- gradient of a cost function w.r.t. the weights of a network
 gradientWeights :: (Floating t) => [Matrix t] -> [Matrix t] -> [Matrix t]
@@ -73,35 +80,47 @@ gradientBiases :: (Floating t) => [Matrix t] -> [Matrix t] -> [Matrix t]
 gradientBiases inputs errors = errors
 
 -- error of the weighted inputs of a network w.r.t. a cost function
-getErrors :: (Floating t) => [(Matrix t, Matrix t)] -> [(Matrix t, Matrix t)] -> Matrix t -> [Matrix t]
+getErrors :: (Floating t) => Network t -> [(Matrix t, Matrix t)] -> Matrix t -> [Matrix t]
 getErrors (l:[]) states label = [errWdInput]
     where errWdInput = elementwise (*) errActivation (foreach sigmoid' zs)
           errActivation = (as - label)
           (zs, as) = head states
-getErrors layers states label = errWdInput:nextErrs
+getErrors net states label = errWdInput:nextErrs
     where errWdInput = elementwise (*) errActivation (foreach sigmoid' zs)
           errActivation = transpose nextWs * (head nextErrs)
-          nextErrs = getErrors (tail layers) (tail states) label
-          (nextWs, nextBs) = head $ tail layers
+          nextErrs = getErrors (tail net) (tail states) label
+          (nextBs, nextWs) = head $ tail net
           (zs, as) = head states
 
 sgdEpoch :: (Floating t) => Network t -> [(Matrix t, Matrix t)] -> t -> [Network t]
 sgdEpoch net [] eta = []
 sgdEpoch net dataset eta = newNet:nextUpdates
     where nextUpdates = sgdEpoch newNet (tail dataset) eta
-          newNet = Network newBiases newWeights
+          newNet = zip newBiases newWeights
           newBiases  = map matsDiff (zip (biases  net) correctionsB)
           newWeights = map matsDiff (zip (weights net) correctionsW)
           matsDiff (m1, m2) = m1 - m2
           correctionsB = map (scaleMatrix eta) (gradientBiases  activs errors)
           correctionsW = map (scaleMatrix eta) (gradientWeights activs errors)
-          errors = getErrors (zip (weights net) (biases net)) states label
+          errors = getErrors net states label
           input  = fst $ head dataset
           label  = snd $ head dataset
           activs = input:(map snd states)
           states = analyze input net
 
+-- infinite list of networks whose parameters are changed with SGD
+sgdUpdates :: (Floating t) => Network t -> [(Matrix t, Matrix t)] -> t -> [Network t]
+sgdUpdates net dataset eta = newNet:nextNets
+    where newNet = last $ sgdEpoch net dataset eta
+          nextNets = sgdUpdates newNet dataset eta
+
+-- average performance of a network on a dataset
 performance :: (Floating t) => Network t -> [(Matrix t, Matrix t)] -> t
 performance net dataset = sum costs / genericLength costs
     where costs = [cost input label | (input, label) <- dataset]
           cost input label = quadCost (infer input net) label
+
+shuffle x = if length x < 2 then return x else do
+    i <- System.Random.randomRIO (0, length(x)-1)
+    r <- shuffle (take i x ++ drop (i+1) x)
+    return (x!!i : r)
--- main.hs
+++ main.hs
@@ -1,19 +1,19 @@
 import qualified Data.Matrix as Mat
 import Neural
 
--- orNet = Network [Mat.fromLists [[-1.0]]] [Mat.fromLists [[2.0, 2.0]]]
--- andNet = Network [Mat.fromLists [[-3.0]]] [Mat.fromLists [[2.0, 2.0]]]
+-- orNet = zip [Mat.fromLists [[-1.0]]] [Mat.fromLists [[2.0, 2.0]]]
+-- andNet = zip [Mat.fromLists [[-3.0]]] [Mat.fromLists [[2.0, 2.0]]]
 -- b11 = Mat.fromLists [[-1], [-1]]
 -- b12 = Mat.fromLists [[-1.25]]
 -- w11 = Mat.fromLists [[2, -2], [-2, 2]]
 -- w12 = Mat.fromLists [[2, 2]]
--- xorNet = Network [b11, b12] [w11, w12]
+-- xorNet = zip [b11, b12] [w11, w12]
 
-b11 = Mat.fromLists [[1.0], [1.0]]
-b12 = Mat.fromLists [[1.0]]
-w11 = Mat.fromLists [[1.0, 1.0], [1.0, 1.0]]
-w12 = Mat.fromLists [[1.0, 1.0]]
-xor1 = Network [b11, b12] [w11, w12]
+b11 = Mat.fromLists [[-0.15269358], [-0.41688649]]
+b12 = Mat.fromLists [[0.26026584]]
+w11 = Mat.fromLists [[1.54684159, -0.45733174], [-1.20085651, -0.11960667]]
+w12 = Mat.fromLists [[-2.05798541, -1.58322432]]
+xor1 = zip [b11, b12] [w11, w12]
 
 input1 = Mat.fromLists [[0.0], [0.0]]
 input2 = Mat.fromLists [[0.0], [1.0]]
@@ -24,8 +24,8 @@ label2 = Mat.fromLists [[1.0]]
 label3 = Mat.fromLists [[1.0]]
 label4 = Mat.fromLists [[0.0]]
 
-dataset = zip [input1, input2, input3, input4] [label1, label2, label3, label4]
--- dataset = zip [input1, input2, input3, input4, input3, input2, input1, input4, input3, input2, input4, input1, input1, input2, input3, input4, input3, input2, input1, input4, input3, input4, input2, input1, input3, input4, input3, input2] [label1, label2, label3, label4, label3, label2, label1, label4, label3, label2, label4, label1, label1, label2, label3, label4, label3, label2, label1, label4, label3, label4, label2, label1, label3, label4, label3, label2]
+-- dataset = zip [input1, input2, input3, input4] [label1, label2, label3, label4]
+dataset = zip [input1, input2, input3, input4, input3, input2, input1, input4, input3, input2, input4, input1, input1, input2, input3, input4, input3, input2, input1, input4, input3, input4, input2, input1, input3, input4, input3, input2] [label1, label2, label3, label4, label3, label2, label1, label4, label3, label2, label4, label1, label1, label2, label3, label4, label3, label2, label1, label4, label3, label4, label2, label1, label3, label4, label3, label2]
 
 -- states = analyze input1 xor1
 -- layers = zip (weights xor1) (biases xor1)
@@ -38,7 +38,7 @@ dataset = zip [input1, input2, input3, input4] [label1, label2, label3, label4]
 -- b22 = b12 - (Mat.scaleMatrix 0.4 (head $ tail nablasB))
 -- w21 = w11 - (Mat.scaleMatrix 0.4 (head nablasW))
 -- w22 = w12 - (Mat.scaleMatrix 0.4 (head $ tail nablasW))
--- xor2 = Network [b21, b22] [w21, w22]
+-- xor2 = zip [b21, b22] [w21, w22]
 
 -- states = analyze input2 xor2
 -- layers = zip (weights xor2) (biases xor2)
@@ -51,7 +51,7 @@ dataset = zip [input1, input2, input3, input4] [label1, label2, label3, label4]
 -- b32 = b22 - (Mat.scaleMatrix 0.4 (head $ tail nablasB))
 -- w31 = w21 - (Mat.scaleMatrix 0.4 (head nablasW))
 -- w32 = w22 - (Mat.scaleMatrix 0.4 (head $ tail nablasW))
--- xor3 = Network [b31, b32] [w31, w32]
+-- xor3 = zip [b31, b32] [w31, w32]
 
 -- states = analyze input3 xor3
 -- layers = zip (weights xor3) (biases xor3)
@@ -64,7 +64,7 @@ dataset = zip [input1, input2, input3, input4] [label1, label2, label3, label4]
 -- b42 = b32 - (Mat.scaleMatrix 0.4 (head $ tail nablasB))
 -- w41 = w31 - (Mat.scaleMatrix 0.4 (head nablasW))
 -- w42 = w32 - (Mat.scaleMatrix 0.4 (head $ tail nablasW))
--- xor4 = Network [b41, b42] [w41, w42]
+-- xor4 = zip [b41, b42] [w41, w42]
 
 -- states = analyze input4 xor4
 -- layers = zip (weights xor4) (biases xor4)
@@ -77,4 +77,4 @@ dataset = zip [input1, input2, input3, input4] [label1, label2, label3, label4]
 -- b52 = b42 - (Mat.scaleMatrix 0.4 (head $ tail nablasB))
 -- w51 = w41 - (Mat.scaleMatrix 0.4 (head nablasW))
 -- w52 = w42 - (Mat.scaleMatrix 0.4 (head $ tail nablasW))
--- xor5 = Network [b51, b52] [w51, w52]
+-- xor5 = zip [b51, b52] [w51, w52]
